a3400c392ef0222fee19f9fa3f68d1d4ede4b194
nasjones <46979749+nasjones@users.noreply.github.com>
2021-11-30 18:20:54 -0500

    Update seed file

--- crowdSource-seed.sql
+++ crowdSource-seed.sql
@@ -1,5 +1,5 @@
 INSERT INTO users (username, PASSWORD, first_name, last_name, email, is_admin)
-  VALUES ('nascreator', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2ki', 'Nas', 'User', 'nas@nasjones.com', FALSE), ('nasadmin', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2ki', 'Nas', 'Jones', 'nas2@nasjones.com', TRUE), ('nasfunder', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2ki', 'Nas', 'Jones', 'nas3@nasjones.com', FALSE);
+  VALUES ('nascreator', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2ki', 'Nas', 'User', 'nas@nasjones.com', FALSE), ('nasadmin', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2ki', 'Nas', 'Jones', 'nas2@nasjones.com', TRUE), ('nasfunder', '$2b$04$3oTLTJia8ErkqD5oOvhVmeT9FVaE9/9Fl59ugrZGGG.O7WJPPv2k', 'Nas', 'Jones', 'nas3@nasjones.com', FALSE);
 
 INSERT INTO products (title, description, amount_sought)
   VALUES ('Crowd Fight', 'A new trading card game where users can submit a photo of  themselves and be entered into the deck.', 200), ('Cat glass', 'A new glass blowing company where the only thing we make is cats', 50);
